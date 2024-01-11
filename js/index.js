5be3944244db9884edb4726a94209151b3ea022c
Swetha Janardhana <SwethaJanardhana85@gmail.com>
2024-01-11 12:03:56 +0100

    task completed

--- js/index.js
+++ js/index.js
@@ -23,10 +23,20 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   let result;
-
-  // --v-- write your code here --v--
-
-  // --^-- write your code here --^--
+  const formData = new FormData(event.target);
+  const dataObject = Object.fromEntries(formData);
+  const NumberA = Number(dataObject.numberA);
+  const NumberB = Number(dataObject.numberB);
+  const operator = dataObject.operator;
+  if (operator === "subtraction") {
+    result = subtract(NumberA, NumberB);
+  } else if (operator === "multiplication") {
+    result = multiply(NumberA, NumberB);
+  } else if (operator === "division") {
+    result = divide(NumberA, NumberB);
+  } else {
+    result = add(NumberA, NumberB);
+  }
 
   resultOutput.textContent = result;
 });
